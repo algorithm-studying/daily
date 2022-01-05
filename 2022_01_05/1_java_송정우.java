b5b9d7bf3a80ef855dec388b87cc39fa3486f089
nice920525@naver.com <nice920525@naver.com>
2022-01-06 00:14:52 +0900

    레벨1_K번째수

--- 2022_01_05/1_java_송정우.java
+++ 2022_01_05/1_java_송정우.java
@@ -28,3 +28,20 @@ class Solution {
         return answer;
     }
 }
+
+/* 다른 사람 풀이
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+
+        return answer;
+    }
+}
+*/
