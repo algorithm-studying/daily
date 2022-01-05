6881cd6ac128bd80be5a58091d978cbaf53dbaca
jo su bin <a011096.aivle@gmail.com>
2022-01-05 23:37:52 +0900

    레벨1_k번째수

---
+++ 2022_01_05/1python_조수빈.py
@@ -0,0 +1,7 @@
+def solution(array, commands):
+    answer = []
+    for c in commands:
+        slice=array[c[0]-1:c[1]]
+        slice.sort()
+        answer.append(slice[c[2]-1])
+    return answer
