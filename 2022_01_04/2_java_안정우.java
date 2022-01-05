9d88801d6b2a4d796dc0f77db9b137a4d48f4b62
jjlicky <74889165+jjlicky@users.noreply.github.com>
2022-01-05 11:02:06 +0900

    레벨3_등굣길

--- 2022_01_04/2_java_안정우.java
+++ 2022_01_04/2_java_안정우.java
@@ -25,3 +25,31 @@ class Solution {
         return answer = check[m][n]%1000000007;
     }
 }
+
+##방법2.메소드사용
+class Solution {
+    static int check[][];
+    public int solution(int m, int n, int[][] puddles) {
+        
+        check = new int[m][n];
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                check[i][j] = -1;
+            }
+        }
+        check[0][0] = 1;
+        for (int[] puddle : puddles)
+            check[puddle[0]-1][puddle[1]-1] = 0;
+     
+        return dp(m-1,n-1)%1000000007;
+    }
+
+    public static int dp(int x, int y) {
+        
+        if (x<0 || y<0) return 0;
+        if (check[x][y] >= 0)
+            return check[x][y];
+        
+        return check[x][y] = dp(x-1,y)%1000000007 + dp(x,y-1)%1000000007;    
+    }
+}
