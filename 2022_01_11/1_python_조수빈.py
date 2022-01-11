7087686a5bd9d72184219791e663f9cfa3648678
jo su bin <a011096.aivle@gmail.com>
2022-01-12 01:00:39 +0900

    레벨1_신규 아이디 추천

--- 2022_01_11/1_python_조수빈.py
+++ 2022_01_11/1_python_조수빈.py
@@ -2,18 +2,24 @@
 
 def solution(new_id):
     answer = ''
+    
     # 1단계
     new_id = new_id.lower()
+    
     # 2단계
     for i in new_id:
         if i.islower() or i.isdigit() or i in ['-', '_', '.']:
            answer += i
+        
     # 3단계
     while '..' in answer:
         answer = answer.replace('..', '.')
+        
     # 4단계
     answer = answer[1:] if answer[0] == '.' and len(answer) > 1 else answer
     answer = answer[:-1] if answer[-1] == '.' else answer
+    
+    
     '''if answer :
           if answer[0] == '.':
                   answer = answer[1:]
@@ -21,15 +27,16 @@ def solution(new_id):
                   answer = answer[:-1]
     '''
 
-
     # 5단계
     if answer == '' :
         answer = 'a'
+        
     # 6단계
     if len(answer) >= 16:
         answer = answer[0:15]
         if answer[-1] == '.':
             answer = answer[0:-1]
+            
     # 7단계
     while len(answer) <= 2:
         answer = answer + answer[-1]
