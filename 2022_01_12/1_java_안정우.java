2d0ed1448bba8b3bfc259b8365325ad65f3b57aa
jjlicky <74889165+jjlicky@users.noreply.github.com>
2022-01-12 12:29:55 +0900

    레벨1_완주하지 못한 선수

---
+++ 2022_01_12/1_java_안정우.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for (int i=0; i<completion.length;i++) {
+            if (!completion[i].equals(participant[i])) {
+                answer = participant[i];
+                break;
+            } 
+        }
+        if (answer=="") return participant[participant.length-1];
+        else return answer;
+    }
+}
