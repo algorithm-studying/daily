25b311b14dd46956cbae6267da6e20db40dc8166
jeongwoo <92703457+songjeongwoo@users.noreply.github.com>
2022-02-07 13:37:04 +0900

    Delete 2022-01-10 directory

--- 2022-01-10/2_python_서승우.py
+++
@@ -1,8 +0,0 @@
-def solution(n):
-    a = ['0', '1', '2', '4']
-    if n <= 3:
-        return a[n]
-    elif n % 3 == 0:
-        return solution((n//3 - 1)) + a[3]
-    else:
-        return solution(n//3) + a[n%3]
