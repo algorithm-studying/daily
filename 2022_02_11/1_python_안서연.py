05961098f96a4ee80599c23821cc7669d74f3040
Seoyeon Ahn <61009770+ahnsun98@users.noreply.github.com>
2022-02-11 11:05:50 +0900

    레벨1_가운데 글자 가져오기

--- 2022_02_11/1_python_안서연.py
+++ 2022_02_11/1_python_안서연.py
@@ -2,10 +2,9 @@ def solution(s):
     answer = ''
     length = len(s)
     
-    li = list(s)
     if(length%2 == 0):
-        answer = li[length//2-1]+li[length//2]
+        answer = s[length//2-1:length//2+1]
     else:
-        answer = li[length//2]
+        answer = s[length//2]
         
     return answer
