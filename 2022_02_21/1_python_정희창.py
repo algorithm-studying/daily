1c3a706b542f211efaa698cbca44ef7488fb46a9
heechang_jeong <63540952+gmlckd1213@users.noreply.github.com>
2022-02-21 22:48:01 +0900

    레벨 1

---
+++ 2022_02_21/1_python_정희창.py
@@ -0,0 +1,7 @@
+def solution(s):
+    answer = []
+    dict = {0 : "zero", 1 : "one", 2 : "two", 3 : "three", 4 : "four", 5 : "five", 6 : "six", 7 : "seven", 8 : "eight", 9 : "nine"}
+    for i in dict.keys():
+            s=s.replace(dict[i],str(i))
+        
+    return int(s)
