4e1c6a42abec74b48c51590d8a592b452428e8c0
Seoyeon Ahn <61009770+ahnsun98@users.noreply.github.com>
2022-02-21 17:41:37 +0900

    레벨3_입국심사(이진탐색)

--- 2022_01_11/3_python_안서연.py
+++ 2022_01_11/3_python_안서연.py
@@ -1,43 +1,19 @@
-# def solution(n, times):
-#     answer = 0
-#     times.sort()
-#     check1 = [0 for i in range(len(times))]
-    
-#     idx = 0
-#     while(n > 0):
-#         if(n == 1):
-#             check2 = check1.copy()
-#             for i in range(len(times)):
-#                 check2[i] += times[i]
-#             break
-#         check1[idx] += times[idx]
-#         idx = check1.index(min(check1))
-#         n -= 1
-    
-#     idx = check2.index(min(check2))
-#     check1[idx] += times[idx]
-#     answer = max(check1)
-
-#     return answer
-
-
 def solution(n, times):
     answer = 0
+    l, r = 1, max(times) * n
     
-    minimum = 0
-    maximum = max(times) * n
-    
-    while(minimum < maximum):
-        mid = (minimum + maximum) // 2 
-        
-        total = 0
+    while(l <= r):
+        mid = (l+r)//2
+        people = 0
         for t in times:
-            total += mid // t
-            
-        if(total >= n):
-            maximum = mid
+            people += mid // t
+            if people >= n:
+                break
+        
+        if people >= n:
+            r = mid - 1
+            answer = mid
         else:
-            minimum = mid + 1
+            l = mid + 1
     
-    answer = minimum
     return answer
