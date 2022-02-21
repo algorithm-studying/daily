d9953d6195564bf5d3abc0a444e7a784cdd5e605
Seoyeon Ahn <61009770+ahnsun98@users.noreply.github.com>
2022-02-21 16:33:14 +0900

    레벨3_네트워크(BFS,DFS)

--- 2022_01_20/3_python_안서연.py
+++ 2022_01_20/3_python_안서연.py
@@ -1,3 +1,4 @@
+########## DFS 재귀 이용 ##########
 def DFS(i, n, computers, node):
     node[i] = True
     for j in range(n):
@@ -11,6 +12,30 @@ def solution(n, computers):
     for i in range(n):
         if node[i] == False:
             DFS(i, n, computers, node)
+            answer += 1
+    
+    return answer
+
+
+########## BFS 스택 이용 ##########
+from collections import deque
+def BFS(i, n, computers, node):
+    q = deque()
+    q.append(i)
+    while q:
+        j = q.popleft()
+        node[j] = True
+        for k in range(n):
+            if computers[j][k] and not node[k]:
+                q.append(k)
+    
+def solution(n, computers):
+    answer = 0
+    node = [False for i in range(n)]
+    
+    for i in range(n):
+        if node[i] == False:
+            BFS(i, n, computers, node)
             answer += 1
     
     return answer
