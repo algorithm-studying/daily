f134da0e87313d4b7b4a094ecccc83fcbd0430a9
songjeongwoo <nice920525@naver.com>
2022-02-28 17:27:54 +0900

    레벨1_약수의 개수와 덧셈

---
+++ 2022_02_28/1_python_송정우.py
@@ -0,0 +1,24 @@
+def solution(left, right):
+    answer = 0
+    for i in range(left, right + 1):
+        cnt = 0
+        for j in range(1, i + 1):
+            if i % j == 0:
+                cnt += 1
+        if cnt % 2 == 0:
+            answer += i
+        else:
+            answer -= i
+    return answer
+
+'''
+다른 사람 풀이:
+def solution(left, right):
+    answer = 0
+    for i in range(left,right+1):
+        if int(i**0.5)==i**0.5:
+            answer -= i
+        else:
+            answer += i
+    return answer
+'''
