8895208ff8cbb9d00f87a498f6b611654eac7deb
english721 <english7212@gmail.com>
2022-05-24 16:23:37 +0900

    레벨1_3진법 뒤집기

---
+++ 2022_05_23/1_python_박유진.py
@@ -0,0 +1,11 @@
+def solution(n):
+    three = []
+    answer = 0
+    # while n > 3으로 해버리면, n = 3일 때 틀림
+    while n > 0 :
+        three.append(n % 3)
+        n //= 3
+    for i in range(len(three)):
+        answer += three[i] * (3**(len(three)-1-i))
+        print(answer)
+    return answer
